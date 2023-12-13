02174a589a816da0f57241c0d00f9873a31fce61
Pavel Larin <www.larinpavel@mail.ru>
2023-12-13 14:41:00 +0500

    LeetCode N-ary Tree Preorder Traversal Solution Explained - Java

--- src/narytreepreordertraversal/Solution.java
+++ src/narytreepreordertraversal/Solution.java
@@ -1,5 +1,7 @@
 package narytreepreordertraversal;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
@@ -52,5 +54,29 @@ public class Solution {
 			children = _children;
 		}
 	};
+	
+	public List<Integer> preorder(Node root) {
+		
+		LinkedList<Node> stack = new LinkedList<>();
+		LinkedList<Integer> output_arr = new LinkedList<>();
+		
+		if (root == null) {
+			return output_arr;
+		}
+		
+		stack.add(root);
+		
+		while(!stack.isEmpty()) {
+			Node node = stack.pollLast();
+			output_arr.add(node.val);
+			Collections.reverse(node.children);
+			for (Node child : node.children) {
+				stack.add(child);
+			}
+		}
+		
+		return output_arr;		
+	}
+	
 
 }

db08fa547f081340c0721a718d371c597f02e9a7
Pavel Larin <www.larinpavel@mail.ru>
2023-12-14 02:02:44 +0500

    Easy. Merge Two Binary Trees Solution

--- src/mergetwobinarytrees/Solution.java
+++ src/mergetwobinarytrees/Solution.java
@@ -45,6 +45,18 @@ public class Solution {
 	
 	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
 		
+		if (t1 == null) {
+			return t2;
+		}
+		
+		if (t2 == null) {
+			return t1;
+		}
+		
+		t1.val += t2.val;
+		t1.left = mergeTrees(t1.left, t2.left);
+		t2.right = mergeTrees(t1.right, t2.right);
+		return t1;		
 	}
 	
 	public static void main(String[] args) {
